f0dbab8559688f2fc3a9b5ad02d9970a7f69ba95
Phil Robibero <pasr.contact@gmail.com>
2022-04-10 10:57:18 -0400

    Added asteroid behavior and spawner

---
+++ Code Lab Group Final/Assets/PhilDev/PhilScripts/AsteroidBehavior.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehavior : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AsteroidMovement();
+    }
+
+    void AsteroidMovement()
+    {
+        float randSpeed = Random.Range(2f, 4f);
+        transform.Translate(Vector2.right * randSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Walls")
+        {
+            Debug.Log("Has exited");
+            Destroy(gameObject);
+
+        }
+    }
+
+    //when hit by player projectile, destory object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Code Lab Group Final/Assets/PhilDev/PhilScripts/AsteroidGenerator.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    [Header("Asteroids")]
+    [SerializeField] GameObject largeAsteroid;
+    [SerializeField] GameObject mediumAsteroid;
+    [SerializeField] GameObject smallAsteroid;
+
+    [Header("Spawner")]
+    [SerializeField] GameObject spawnOne;
+    [SerializeField] GameObject spawnTwo;
+    [SerializeField] GameObject spawnThree;
+    [SerializeField] GameObject spawnFour;
+
+    [Header("Asteroid Timer")]
+    [SerializeField] float asteroidTimer = 2f;
+    float elapsedTime;
+
+    private List<Vector2> spawnLocations = new List<Vector2>();
+    private Quaternion newRotation;
+    Vector2 newPosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocations.Add(spawnOne.transform.position);
+        spawnLocations.Add(spawnTwo.transform.position);
+        spawnLocations.Add(spawnThree.transform.position);
+        spawnLocations.Add(spawnFour.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= asteroidTimer)
+        {
+            elapsedTime = 0;
+            GenerateAsteroid();
+        }
+    }
+
+    //Check how many asteroids are in game
+    //If it's less than 10, GenerateAsteroid
+    bool AsteroidChecker()
+    {
+        if (GameObject.FindGameObjectsWithTag("Asteroid").Length >= 10)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    //Set health for Asteroid
+    int SetAsteroidHealth()
+    {
+        int asteroidHealth = Random.Range(1,4);
+        return asteroidHealth;
+    }
+
+    //create a random number used to determine
+    //the spawner location and rotation
+    int RandomSpawnerProperty()
+    {
+        int randSpawnNum = Random.Range(0, 4);
+        return randSpawnNum;
+    }
+
+    float RandomRotation()
+    {
+        float randomRot = Random.Range(-10f, 10f);
+        return randomRot;
+    }
+
+    //Generate random to set spawning point
+    Vector2 GenerateNewPosition(int randSpawnNum)
+    {
+        return  newPosition = spawnLocations[randSpawnNum];
+    }
+
+    //set random rotation
+    Quaternion SetRandomRotation(int randSpawnNum)
+    {
+
+        if (randSpawnNum == 1)
+        {
+            newRotation = Quaternion.Euler(0, 0, spawnOne.transform.rotation.eulerAngles.z + RandomRotation());
+        }
+        else if (randSpawnNum == 2)
+        {
+            newRotation = Quaternion.Euler(0, 0, spawnTwo.transform.rotation.eulerAngles.z + RandomRotation());
+        }
+        else if (randSpawnNum == 3)
+        {
+            newRotation = Quaternion.Euler(0, 0, spawnThree.transform.rotation.eulerAngles.z + RandomRotation());
+        }
+        else if (randSpawnNum == 4)
+        {
+            newRotation = Quaternion.Euler(0, 0, spawnFour.transform.rotation.eulerAngles.z + RandomRotation());
+        }
+        return newRotation;
+    }
+
+    //Generate asteroid based on SetAsteroidHealth randomizer
+    void GenerateAsteroid()
+    {
+
+        if (AsteroidChecker())
+    {
+            if (SetAsteroidHealth() == 3)
+            {
+                GameObject.Instantiate(largeAsteroid, GenerateNewPosition(RandomSpawnerProperty()), SetRandomRotation(RandomSpawnerProperty()));
+
+            }
+            else if (SetAsteroidHealth() == 2)
+            {
+                GameObject.Instantiate(mediumAsteroid, GenerateNewPosition(RandomSpawnerProperty()), SetRandomRotation(RandomSpawnerProperty()));
+            }
+            else if (SetAsteroidHealth() == 1)
+            {
+                GameObject.Instantiate(smallAsteroid, GenerateNewPosition(RandomSpawnerProperty()), SetRandomRotation(RandomSpawnerProperty()));
+            }
+            else
+            {
+            }
+        }
+    }
+}
+
